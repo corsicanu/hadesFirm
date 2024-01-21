752da718f3960e105a67ec0e415e80b8c3078880
corsicanu <me@corsicanu.ro>
2024-01-21 07:14:53 +0200

    Revert "Project: .net8.0 and vscode2022"
    -we be doing a separate branch for this
    
    This reverts commit a6b993ed024e4f484fc7e13fdfaeef62ce109219.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,7 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hadesFirm Reborn")]
+[assembly: AssemblyFileVersion("0.3.6.8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright zxz0O0 ©  2015 / Ivan Meler 2021-2023 / KSmith 2023 / corsicanu 2024")]
+[assembly: AssemblyTitle("hadesFirm Reborn")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyFileVersion("0.3.6.8")]
 [assembly: Guid("f64041a3-90f5-4bee-a430-1c0f1d604eee")]
+[assembly: AssemblyVersion("1.0.0.1")]
